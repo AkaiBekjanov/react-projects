c6b04ef972d8d52ac1ebca1b13ae147ebc2a7c92
AkaiBekjanov <bekjanov.akai.official@gmail.com>
2023-04-08 23:03:06 +0600

    added git pages

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -8,7 +8,7 @@ export const Note=(props)=>{
         <div className="note"  style={{backgroundColor:"rgba(11,244,99)"}}>
             <p>{props.text}</p>
             <div className="note-footer">
-                <span className="data">08.04.23</span>
+                <span className="data">09.04.23</span>
                 <span className="delete-icon" onClick={props.deleteNote}>X</span>
             </div>
         </div>
